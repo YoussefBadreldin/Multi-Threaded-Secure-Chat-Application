989d20867f6b49fabb52804e88762fbb2198d381
Youssef Badreldin <youssefmbadreldin@gmail.com>
2023-12-25 11:44:32 +0200

    updates

--- src/ChatApplication/Client.java
+++ src/ChatApplication/Client.java
@@ -1,22 +1,22 @@
-//package test1;
+//package ChatApplication;
 //import java.io.*;
 //import java.net.*;
 //import java.util.Scanner;
 //public class Client {
-//	
+//
 //	    private static  String SERVER_ADDRESS = "127.0.0.2"; // Replace with the server's IP address
 //	    private static final int PORT = 5000;
-//	
-//	    
+//
+//
 //		public static void main(String[] args) {
 //	        try (
-//	        		
+//
 //	            Socket socket = new Socket(SERVER_ADDRESS, PORT);
 //	            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 //	            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 //	            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in))
 //	        ) {
-//	        	
+//
 //	            System.out.println("Connected to the server." );
 //
 //	            Thread receiveThread = new Thread(() -> {
